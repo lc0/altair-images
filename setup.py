5293d3db1b7260d9853050c25a6cbdd5b8821da1
Sergii Khomenko <sergii.khomenko@stylight.com>
2020-04-14 23:47:08 +0200

    Simple setup.py; add notebook

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="altair-images",
+    version="0.0.2",
+    author="Sergii Khomenko",
+    description="Serving interactive charts with images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lc0/altair_images",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='altair flask colab ngrok demo',
+    install_requires=required,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+)
---
+++ src/altair_images/thread.py
@@ -0,0 +1,33 @@
+from flask_ngrok import _run_ngrok
+import threading
+
+class ServerThread(threading.Thread):
+
+    def __init__(self, app, host, port):
+        threading.Thread.__init__(self)
+        self.srv = make_server(host, port, app)
+        self.ctx = app.app_context()
+        self.ctx.push()
+
+    def run(self):
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        self.srv.shutdown()
+
+def start_flask_thread(server_thread, app, host='0.0.0.0', port=5555, ngrok=False):
+    if server_thread:
+        print(" * Stopping current thread")
+        server_thread.shutdown()
+
+    # running fist time, so we should start ngrok
+    # TODO: check on what port we have running ngrok's
+    if server_thread is None and ngrok:
+            ngrok_address = _run_ngrok(port)
+            print(f" * Running on {ngrok_address}")
+
+    server_thread = ServerThread(app, host=host, port=port)
+    server_thread.start()
+    print(f" * Started a server thread on {host}:{port}")
+
+    return server_thread

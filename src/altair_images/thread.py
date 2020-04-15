219551748330d43df31d8b67fe52609793e4dac8
Sergii Khomenko <sergii.khomenko@stylight.com>
2020-04-15 08:55:32 +0200

    Clean up start_flask_thread

--- src/altair_images/thread.py
+++ src/altair_images/thread.py
@@ -5,12 +5,23 @@ from flask_ngrok import _run_ngrok
 
 
 class ServerThread(threading.Thread):
+    pool = {}
 
     def __init__(self, app, host, port):
         threading.Thread.__init__(self)
+
+        if port in self.pool:
+            print(" * Re-using server on the same port")
+            self.srv = self.pool[port]
+            self.srv.shutdown()
+
         self.srv = make_server(host, port, app)
         self.ctx = app.app_context()
         self.ctx.push()
+
+        self.pool[port] = self.srv
+
+
 
     def run(self):
         self.srv.serve_forever()
@@ -18,14 +29,11 @@ class ServerThread(threading.Thread):
     def shutdown(self):
         self.srv.shutdown()
 
-def start_flask_thread(server_thread, app, host='0.0.0.0', port=5555, use_ngrok=False):
-    if server_thread:
-        print(" * Stopping current thread")
-        server_thread.shutdown()
+def start_flask_thread(app, host='0.0.0.0', port=5555, use_ngrok=False):
 
     # running fist time, so we should start ngrok
     # TODO: check on what port we have running ngrok's
-    if server_thread is None and use_ngrok:
+    if use_ngrok:
             ngrok_address = _run_ngrok(port)
             print(f" * Running on {ngrok_address}")
 
@@ -33,4 +41,4 @@ def start_flask_thread(server_thread, app, host='0.0.0.0', port=5555, use_ngrok=
     server_thread.start()
     print(f" * Started a server thread on http://{host}:{port}")
 
-    return server_thread
+    return server_thread, f"http://{host}:{port}"
